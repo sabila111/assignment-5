780c2c9cdb58e75756a06f3615e2d9c5746c6375
sabila111 <sharminsharker2002@gmail.com>
2024-09-25 10:59:06 +0600

    finished most of the work

--- js/index.js
+++ js/index.js
@@ -14,14 +14,22 @@ donateButton.addEventListener('click', function(){
     const donatedTotalMoney = donation + donatedMoney
     document.getElementById('donated-money').innerText = donatedTotalMoney
 
-    const minusMoney = totalMoney-donatedMoney
+    const minusMoney = totalMoney-donation
     document.getElementById('total-money').innerText= minusMoney
      
     // show modal
     modal.showModal()
    
-    console.log( minusMoney)
-
+    // history section
+    const dt = new Date()
+    const noakhali = document.getElementById('noakhali-flood').innerText
+    const historyContainer = document.createElement('div')
+    historyContainer.className=' drop-shadow-lg border-2 p-8 rounded-xl mb-4'
+    historyContainer.innerHTML= `
+    <h3 class="font-bold text-xl mb-2">${donation} BDT is donated for ${noakhali}  </h3>
+    <p class="text-gray-500">Date: ${dt} </p>
+    `
+    document.getElementById('history-container').appendChild(historyContainer)
     }
     else{
         alert("Please enter a valid donation amount");
@@ -48,13 +56,24 @@ donateButton2.addEventListener('click', function(){
         const donatedTotalMoney2 = donation2 + donatedMoney2
         document.getElementById('donated-money2').innerText = donatedTotalMoney2
 
-        const minusMoney2 = totalMoney2-donatedMoney2
+        const minusMoney2 = totalMoney2-donation2
         document.getElementById('total-money').innerText= minusMoney2
 
         // show modal
     modal.showModal()
 
-        console.log(minusMoney2 )
+       
+ // history section
+ const dt = new Date()
+ const feni = document.getElementById('feni-flood').innerText
+ const historyContainer2 = document.createElement('div')
+ historyContainer2.className=' drop-shadow-lg border-2 p-8 rounded-xl mb-4'
+ historyContainer2.innerHTML= `
+ <h3 class="font-bold text-xl mb-2">${donation2} BDT is donated for ${feni}  </h3>
+ <p class="text-gray-500">Date: ${dt} </p>
+ `
+ document.getElementById('history-container').appendChild(historyContainer2)
+
     }
     else{
         alert("Please enter a valid donation amount");
@@ -81,12 +100,25 @@ donateButton3.addEventListener('click', function(){
     const donatedTotalMoney3 = donation3 + donatedMoney3
     document.getElementById('donated-money3').innerText = donatedTotalMoney3
 
-    const minusMoney3 = totalMoney3-donatedMoney3
+    const minusMoney3 = totalMoney3-donation3
     document.getElementById('total-money').innerText= minusMoney3
 
     // show modal
 modal.showModal()
-    console.log( minusMoney3)
+   
+// history section
+const dt = new Date()
+const quota = document.getElementById('quota-movement').innerText
+const historyContainer3 = document.createElement('div')
+historyContainer3.className=' drop-shadow-lg border-2 p-8 rounded-xl mb-4'
+historyContainer3.innerHTML= `
+<h3 class="font-bold text-xl mb-2">${donation3} BDT is donated for ${quota}  </h3>
+<p class="text-gray-500">Date: ${dt} </p>
+`
+document.getElementById('history-container').appendChild(historyContainer3)
+
+
+
     }
 
     else{
@@ -98,3 +130,36 @@ modal.showModal()
     })
     
 })
+
+
+// history tab
+
+
+const historyTab = document.getElementById('history-tab')
+const donationTab = document.getElementById('donation-tab')
+historyTab.addEventListener('click', function(){
+
+    historyTab.classList.add('bg-lime-300')
+    historyTab.classList.remove('border-2')
+
+    donationTab.classList.remove('bg-lime-300')
+    donationTab.classList.add('border-2')
+
+    document.getElementById('donation-section').classList.add('hidden')
+    document.getElementById('history-section').classList.remove('hidden')
+    console.log("Donation section hidden, history section shown");
+})
+
+donationTab.addEventListener('click', function() {
+    donationTab.classList.add('bg-lime-300');
+    donationTab.classList.remove('border-2');
+
+    historyTab.classList.remove('bg-lime-300');
+    historyTab.classList.add('border-2');
+
+    document.getElementById('donation-section').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
+    console.log("Donation section shown, history section hidden");
+});
+
+
